0ee884d89ce6821bb4381e1a914152f2603c36d1
Abhishek Kr. Prajapati <157404157+abhishek-0079@users.noreply.github.com>
2024-07-03 22:16:51 +0530

    Add files via upload

---
+++ BASICS/Basic Programs/3_Adding_two_No.cpp
@@ -0,0 +1,14 @@
+//Program to add two numbers
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter two numbers";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"Addition is"<<c;
+
+    return 0;
+}
