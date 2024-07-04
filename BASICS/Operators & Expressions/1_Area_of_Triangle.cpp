194e7f7b8fc6d211be0e9908d50a31018d6a250b
Abhishek Kr. Prajapati <157404157+abhishek-0079@users.noreply.github.com>
2024-07-04 20:25:22 +0530

    Create 1_Area_of_Triangle.cpp

---
+++ BASICS/Operators & Expressions/1_Area_of_Triangle.cpp
@@ -0,0 +1,15 @@
+//Program to find Area of Triangle
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float b,h,a;
+    cout<<"Enter base and height"<<endl;
+    cin>>b>>h;
+
+    a=(b*h)/2;
+
+    cout<<"Area of triangel is "<<a;
+    return 0;
+}
